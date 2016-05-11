371b4bc9c9a41dda55365f0a986c4c6547e32d64
kindlingscript <inflamedmuse@gmail.com>
2016-05-10 23:56:36 -0500

    Counter works now + other style / JS modifications

--- js/app.js
+++ js/app.js
@@ -1,22 +1,19 @@
 "use strict"; 
 
-  /*--- Functions for app ---*/
+/*--- Functions for app ---*/
 
-  // Function to come up with magic number.
-  function getMagicNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+// Function to come up with magic number.
+function getMagicNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-  // Function to make sure input is between 1 - 100
-  function checkInputValidity(input) {
-    if (input < 1 || input > 100) {
-      $("#feedback").html("Please enter a number between 1 and 100.");
-    } 
-  }
-
-  // Function to find difference between user input and magic number.
-  // See how close or far away they are from guessing right.
-  function getDiff(input, magicNumber) {
+// Function to find difference between user input and magic number.
+// See how close or far away they are from guessing right.
+function getDiff(input, magicNumber) {
+  // Check for valid input first.
+  if (input <= 1 || input >= 100) {
+    $("#feedback").html("Please enter a number between 1 and 100.");
+  } else {
     var diff = input - magicNumber;
     if (diff == 0) {
       $("#feedback").html("Sizzling! You got it!");
@@ -34,38 +31,30 @@
       $("#feedback").html("Freezing cold!");
     }
   }
+  $("#userGuess").val("").focus();
+}
 
-  /***
-  If there is a difference between user guess and magic number, 
-  then log their guess in #guessList and increment guess count (#count) by 1. 
-  ***/
-  function logGuesses(input, magicNumber) {
-    // var guessCount = document.getElementById("span#count");
-    if (input != magicNumber) {
-      $("#guessList").append("<li>" + input + "</li>");
-      // counter += 1;
-      // $("#count").html("<span>" + counter + "</span>");
-      // console.log("Looping!");
-    }
+/* If there is a difference between user guess and magic number, then log their guess in #guessList and increment guess count (#count) by 1. */
+function logGuesses(input, magicNumber) {
+  if (input != magicNumber) {
+    $("#guessList").append("<li>" + input + "</li>");
   }
+}
 
-  // Every time the user submits input, increment guess count until they get the magicNumber.
-  function incrementGuessCount(input, magicNumber) {
-    // for (var counter = 0; input != magicNumber; counter++) {
-    //   counter += 1;
-    //   $("#count").html("<span>" + counter + "</span>");
-    var counter = 0;
-    if (input != magicNumber) {
-      counter += 1;
-      $("#count").html("<span>" + counter + "</span>");
-    }
+// Every time the user submits input, increment guess count until they get the magicNumber.
+var counter = 0;
+function incrementGuessCount(input, magicNumber) {
+  if (input != magicNumber) {
+    counter++;
+    $("#count").html("<span>" + counter + "</span>");
   }
+}
 
-  // Render new game when user click on "+ New Game" link.
-  // Will need to reset html to original, clear everything.
-  // function newGame() {
-  //   var magicNumber = getMagicNumber(1, 100);
-  // }
+// Render new game when user click on "+ New Game" link.
+// Will need to reset html to original, clear everything.
+// function newGame() {
+//   var magicNumber = getMagicNumber(1, 100);
+// }
 
 $(document).ready(function(){
   var magicNumber = getMagicNumber(1, 100);
@@ -80,25 +69,16 @@ $(document).ready(function(){
 		$(".overlay").fadeOut(1000);
 	});
 
-  // Start guess count at 0.
-  // var counter = 0;
-
-  // $("form").on("click", "#guessButton", function(e) {
-  //   e.preventDefault();
-  //   var input = $("#userGuess").val();
-  //   if (input != magicNumber) {
-  //     $("#count").html("<span>" + counter + "</span>")
-  //     counter += 1;
-  //   }
-  // }
-
   // To run when the user enters a number.
-  $("form").submit(function(e) {
+  $("#guessButton").click(function(e) {
     e.preventDefault();
     var input = $("#userGuess").val();
-    checkInputValidity(input);
-    getDiff(input, magicNumber);
-    logGuesses(input, magicNumber);
-    incrementGuessCount(input, magicNumber);
+    if (input == '') {
+      alert("Please enter a number first.");
+    } else {
+      getDiff(input, magicNumber);
+      logGuesses(input, magicNumber);
+      incrementGuessCount(input, magicNumber);
+    }
   })
 });
