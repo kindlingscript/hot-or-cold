1c060216c736b3b6eca82307448918859fad0d9f
kindlingscript <inflamedmuse@gmail.com>
2016-05-12 21:46:58 -0500

    JS refactoring

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
 "use strict"; 
+var magicNumber;
+var input;
+var guessedNums;
+var lastGuess = "";
+var counter;
 
 /*--- Functions for app ---*/
 
@@ -8,25 +13,27 @@ function getMagicNumber(min, max) {
 }
 
 /* Function to find difference between user input and magic number. See how close or far away they are from guessing right. */
-var guessedNums = []; // Global variables
-var lastGuess;
 function getDiff(input, magicNumber) {
+  console.log("guessedNums: " + guessedNums);
   // See if input has already been entered.
   for (var i = 0; i < guessedNums.length; i++) {
-    if (input == guessedNums[i]) {
+    if (input === guessedNums[i] && guessedNums != null) {
       alert("You've already guessed that number. Try another!");
+    } else {
+      logGuesses();
     }
-  }
-  guessedNums.push(input);
-
+  };
+  
   if (input <= 1 || input >= 100) {
     $("#feedback").html("Please enter a number between 1 and 100.");
   } else {
+    guessedNums.push(input);
     // Variables to get difference between current input and magicNum.
     var smallerNum = Math.min(input, magicNumber);
     var largerNum = Math.max(input, magicNumber);
     var diff = largerNum - smallerNum;
-
+    console.log("diff: " + diff);
+    console.log("lastGuess: " + lastGuess);
     // Variables to get diff between the last guessed number and magicNum.
     var lastGuessSmallerNum = Math.min(lastGuess, magicNumber);
     var lastGuessLargerNum = Math.max(lastGuess, magicNumber);
@@ -36,7 +43,7 @@ function getDiff(input, magicNumber) {
     if (diff == 0) {
       $("#feedback").html("Sizzling! You got it!");
     } else if (diff >= 1 && diff < 10) { 
-      if (diff < lastGuessDiff) {
+      if (diff < lastGuessDiff && lastGuess != "") {
         $("#feedback").html("HOT! You're closing in! Your guess is closer now.");
       } else if (diff > lastGuessDiff) {
         $("#feedback").html("HOT! You're closing in! Your guess was closer before.");
@@ -85,8 +92,8 @@ function getDiff(input, magicNumber) {
       }
     }
   };
-  $("#userGuess").val("").focus();
-  lastGuess = input;
+  console.log("input: " + input);
+  // $("#userGuess").val("").focus();
 };
 
 /* If there is a difference between user guess and magic number, then log their guess in #guessList and increment guess count (#count) by 1. */
@@ -97,7 +104,6 @@ function logGuesses(input, magicNumber) {
 };
 
 // Every time the user submits input, increment guess count until they get the magicNumber.
-var counter = 0;
 function incrementGuessCount(input, magicNumber) {
   if (input != magicNumber && (input >= 1 && input <= 100)) {
     counter++;
@@ -107,18 +113,31 @@ function incrementGuessCount(input, magicNumber) {
 
 // Render new game when user click on "+ New Game" link.
 function newGame() {
-  $("#count").html("<span>" + 0 + "</span>");
+  $("#count").html("<span>0</span>");
   counter = 0;
   $("#guessList").html("");
   $("#feedback").html("Make your guess!");
   guessedNums = [];
   magicNumber = getMagicNumber(1, 100);
-  input = '';
+  lastGuess = "";
+  console.log("magicNumber: " + magicNumber);
+  $("#guessButton").click(function(e) {
+    e.preventDefault();
+    input = $("#userGuess").val();
+    // if (input == '') {
+    //   alert("Please enter a number first.");
+    // } else {
+      getDiff(input, magicNumber);
+      // console.log(getDiff);
+      logGuesses(input, magicNumber);
+      // console.log(logGuesses);
+      incrementGuessCount(input, magicNumber);
+      // console.log(incrementGuessCount);
+    // }
+  });
 };
 
 $(document).ready(function(){
-  var magicNumber = getMagicNumber(1, 100);
-
   /*--- Display information modal box ---*/
 	$(".what").click(function(){
   	$(".overlay").fadeIn(1000);
@@ -129,18 +148,20 @@ $(document).ready(function(){
 		$(".overlay").fadeOut(1000);
 	});
 
+  newGame();
+
   // To run when the user enters a number.
-  $("#guessButton").click(function(e) {
-    e.preventDefault();
-    var input = $("#userGuess").val();
-    if (input == '') {
-      alert("Please enter a number first.");
-    } else {
-      getDiff(input, magicNumber);
-      logGuesses(input, magicNumber);
-      incrementGuessCount(input, magicNumber);
-    }
-  });
+  // $("#guessButton").click(function(e) {
+  //   e.preventDefault();
+  //   var input = $("#userGuess").val();
+  //   if (input == '') {
+  //     alert("Please enter a number first.");
+  //   } else {
+  //     getDiff(input, magicNumber);
+  //     logGuesses(input, magicNumber);
+  //     incrementGuessCount(input, magicNumber);
+  //   }
+  // });
 
   $("a.new").click(function() {
     newGame();
